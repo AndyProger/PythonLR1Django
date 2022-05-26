6fdb3cd153ddbd85b91b44ba66ae5c0291134b9d
AndyProger <84966031+AndyProger@users.noreply.github.com>
2022-05-27 00:11:46 +0300

    lab 1 is finished

--- lr1/lr1/settings.py
+++ lr1/lr1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'lr1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'links',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
 
