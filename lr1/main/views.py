c85cccb348aa210ab645d8078c182ca30a221cef
AndyProger <84966031+AndyProger@users.noreply.github.com>
2022-05-26 23:29:30 +0300

    almostfinished

--- lr1/main/views.py
+++ lr1/main/views.py
@@ -3,5 +3,10 @@ from .models import Links
 
 
 def index(request):
+    if request.method == 'POST':
+        obj = Links.objects.get(linkURL=request.POST["name-of-field"])
+        obj.counter += 1
+        obj.save()
+
     links = Links.objects.all()
     return render(request, 'main/index.html', {'links': links})
